e5bb92cfb88f3b19d5f762e46388b222ac7b7994
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2019-05-07 02:40:42 +0900

    Impl lesson 6

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,55 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by shimastripe on 2019/05/07.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    // MARK: Private Methods
+    private func setupButtons() {
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constaints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to rating button array
+            ratingButtons.append(button)
+        }
+    }
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
 
     override func viewDidLoad() {
@@ -22,6 +23,18 @@ class ViewController: UIViewController {
     }
 
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        self.nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library
+        let imagePickController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickController.sourceType = .photoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickController.delegate = self
+        present(imagePickController, animated: true, completion: nil)
+    }
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         self.mealNameLabel.text = "Default Text"
     }
@@ -38,5 +51,23 @@ extension ViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         self.mealNameLabel.text = textField.text
+    }
+}
+
+// MARK: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user cancelled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // Set photoImageView to display the selectedImage
+        photoImageView.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
 }
