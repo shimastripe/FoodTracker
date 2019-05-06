50e925219306e3c4a405667bcb61aaefe900d751
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2019-05-06 14:45:07 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by shimastripe on 2019/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
