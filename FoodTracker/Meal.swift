5b1616aea3e7b3b494fca6ce924d66cdc66b79e8
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2020-01-05 17:56:54 +0900

    Impl lesson 10

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSSecureCoding {
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo, forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal Object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
 
     // MARK: Properties
 
     var name: String
     var photo: UIImage?
     var rating: Int
+
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+
+    // MARK: Types
+
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
 
     // MARK: Initialization
 
@@ -34,4 +67,7 @@ class Meal {
         self.photo = photo
         self.rating = rating
     }
+
+    // MARK: NSSecureCoding
+    static var supportsSecureCoding = true
 }
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -28,13 +28,30 @@ class MealViewController: UIViewController {
         // Handle the text field's user input through delegate callbacks.
         self.nameTextField.delegate = self
 
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
     }
 
     // MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owingNavigationController = navigationController {
+            owingNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
 
     // This method lets you configure a view controller before it's presented.
